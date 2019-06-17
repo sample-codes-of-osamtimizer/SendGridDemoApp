b03548777dc5fd9e7c73a845d8034d7f73534242
osamtimizer <eaten.jb@gmail.com>
2019-06-18 02:08:35 +0900

    configure letter_opener_web

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'hello#index'
   get 'hello/index'
   resources :users
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
 end
