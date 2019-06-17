3934f0746763c98f44c25c711520390beeba011d
osamtimizer <eaten.jb@gmail.com>
2019-06-18 01:55:55 +0900

    add users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email)
