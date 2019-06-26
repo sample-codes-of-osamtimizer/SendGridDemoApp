c1e95c6254c6c6ee02948379baa82c35e08e2bde
osamtimizer <eaten.jb@gmail.com>
2019-06-27 02:17:01 +0900

    forgot deliver_now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
+      UserMailer.welcome_email(user).deliver_now
       redirect_to root_path
     else
       flash[:error] = 'Failed to save user.'
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,6 @@
 class UserMailer < ApplicationMailer
+  default from: 'test@example.com'
+
   def welcome_email(user)
     mail(to: user.email, subject: 'Confirm Your Account')
   end
