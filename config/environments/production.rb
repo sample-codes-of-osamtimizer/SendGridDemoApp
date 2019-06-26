dd20df33cadab4409a62695356f1de06645d6c7a
osamtimizer <eaten.jb@gmail.com>
2019-06-27 02:25:12 +0900

    add production settings for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Email settings
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => ENV['SENDGRID_DOMAIN'],
+    :address => ENV['SENDGRID_ADDRESS'],
+    :port => ENV['SENDGRIPD_PORT'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
