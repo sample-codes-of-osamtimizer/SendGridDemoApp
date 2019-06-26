7473243f59e8880025182316b930f0da48411490
osamtimizer <eaten.jb@gmail.com>
2019-06-27 02:17:52 +0900

    add user_root_path on mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'test@example.com'
 
   def welcome_email(user)
+    @user = user
     mail(to: user.email, subject: 'Confirm Your Account')
   end
 end
